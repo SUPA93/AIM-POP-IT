14c82b42fb74a1e6c6d3c2559345f4e3d76bb5f5
SUPA93 <supaflavaz93@hotmail.com>
2024-02-12 23:08:21 +0100

    POP THEM ALL

---
+++ index.js
@@ -0,0 +1,68 @@
+const counterDisplay = document.querySelector("h3")
+const timeDisplay = document.querySelector("h5")
+const startButton = document.querySelector("#start");
+let counter = 0;
+let time = 60;
+let isGameActive = false;
+
+const startGame = () => {
+    counter = 0;
+    time = 60;
+    isGameActive = true;
+    counterDisplay.textContent = counter;
+    timeDisplay.textContent = time;
+    startButton.style.display = "none";
+
+    const bubbleMaker = () => {
+        const bubble = document.createElement("span");
+        bubble.classList.add("bubble");
+
+        document.body.appendChild(bubble);
+
+        const size = Math.random() * 100 + 100 + "px";
+        bubble.style.height = size;
+        bubble.style.width = size;
+
+        bubble.style.top = Math.random() * 100 + 100 + "%";
+        bubble.style.left = Math.random() * 100 + "%";
+
+        const plusMinus = Math.random() > 0.5 ? 1 : -1;
+        bubble.style.setProperty('--left', Math.random(plusMinus) * 100 + "%");
+
+        bubble.addEventListener('click', () => {
+            if (isGameActive) {
+                bubble.remove();
+                counter++;
+                counterDisplay.textContent = counter;
+            }
+        });
+
+        setTimeout(() => {
+            bubble.remove();
+        }, 8000)
+    };
+    const timerInterval = setInterval(() => {
+        time--;
+        timeDisplay.textContent = time;
+
+        if (time <= 0) {
+            clearInterval(timerInterval);
+            isGameActive = false;
+        }
+    }, 1000);
+
+    const bubbleInterval = setInterval(bubbleMaker, 600);
+    const clearBubbles = () => {
+        document.querySelectorAll('.bubble').forEach(bubble => {
+            bubble.remove();
+        });
+    };
+    setTimeout(() => {
+        clearInterval(bubbleInterval);
+        clearBubbles();
+        startButton.style.display = "";
+    }, 60000);
+
+};
+startButton.addEventListener('click', startGame);
+timeDisplay.textContent = `${time}`;
